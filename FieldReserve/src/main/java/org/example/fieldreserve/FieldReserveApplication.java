047472b23fb1ea4e21b694121774f6cf4e294959
ArazHafez2005 <ah5837@rit.edu>
2026-02-18 00:41:46 +0400

    Environment for our application - Made on Intellij

---
+++ FieldReserve/src/main/java/org/example/fieldreserve/FieldReserveApplication.java
@@ -0,0 +1,13 @@
+package org.example.fieldreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldReserveApplication.class, args);
+    }
+
+}
