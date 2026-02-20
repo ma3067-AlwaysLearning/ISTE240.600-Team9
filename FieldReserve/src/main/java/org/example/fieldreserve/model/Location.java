d6be07fa0f2c148fbcbd480cf54084ea8573eaf3
ma3067-AlwaysLearning <ma3067@rit.edu>
2026-02-20 21:46:38 +0400

    Updated Location.java file
    Signed-off-by: ma3067-AlwaysLearning <ma3067@rit.edu>

--- FieldReserve/src/main/java/org/example/fieldreserve/model/Location.java
+++ FieldReserve/src/main/java/org/example/fieldreserve/model/Location.java
@@ -1,6 +1,5 @@
 package org.example.fieldreserve.model;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ import java.util.ArrayList;
 @Component
 public class Location {
     private int locationID; // Unique identifier for a location area
-    private ArrayList<Field> fields; //Holds all fields that belong in a single location
+    private ArrayList<Field> fields = new ArrayList<>(); //Holds all fields that belong in a single location
     private String locationName; // A name given for the location
     private String locationArea; // A place inside a city, like Dubai Silicon Oasis
     private String locationCity; // Cities like Dubai,Sharjah and so ...
