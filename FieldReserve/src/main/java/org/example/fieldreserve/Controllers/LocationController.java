2220caf8ecc45cb8cbb1a4275a4ec29986bc307a
ma3067-AlwaysLearning <ma3067@rit.edu>
2026-04-26 13:05:58 +0400

    Reimplemented Location Controller to be a REST controller
    Signed-off-by: ma3067-AlwaysLearning <ma3067@rit.edu>

--- FieldReserve/src/main/java/org/example/fieldreserve/Controllers/LocationController.java
+++ FieldReserve/src/main/java/org/example/fieldreserve/Controllers/LocationController.java
@@ -1,34 +1,49 @@
 package org.example.fieldreserve.Controllers;
 
-import org.example.fieldreserve.Services.DataService;
+import org.example.fieldreserve.Services.LocationService;
+import org.example.fieldreserve.model.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/locations")
 public class LocationController {
 
     @Autowired
-    private DataService dataService;
+    private LocationService locationService;
 
-    //View all locations
-    @GetMapping("/locations")
-    public String showLocations(Model model) {
-        model.addAttribute("locations", dataService.getAllLocations());
-        return "location-datatable";
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
     }
 
-    //Show form
-    @GetMapping("/locations/add")
-    public String showAddLocationForm() {
-        return "location_form";
+    @GetMapping("/{id}")
+    public Optional<Location> getLocationById(@PathVariable int id) {
+        return locationService.getLocationById(id);
     }
 
-    // Process form
-    @PostMapping("/locations/add")
-    public String addLocation(@RequestParam int id, @RequestParam String name) {
-        dataService.addLocation(id, name);
-        return "redirect:/locations";
+    @GetMapping("/citySearch")
+    public List<Location> searchByCity(@RequestParam String city){
+        return locationService.findByCity(city);
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location){
+        return locationService.saveLocation(location);
+    }
+
+    @PutMapping("/{id}")
+    public Location updateLocation(@PathVariable Long id, @RequestBody Location location){
+        return locationService.updateLocation(id, location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocationById(@PathVariable long id){
+        locationService.deleteLocationById(id);
     }
 }
