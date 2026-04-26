b6269bc42815048ad0d57d990d628922c1d7c296
ma3067-AlwaysLearning <ma3067@rit.edu>
2026-04-26 12:54:31 +0400

    Added location service
    Signed-off-by: ma3067-AlwaysLearning <ma3067@rit.edu>

---
+++ FieldReserve/src/main/java/org/example/fieldreserve/Services/LocationService.java
@@ -0,0 +1,43 @@
+package org.example.fieldreserve.Services;
+
+import jakarta.transaction.Transactional;
+import org.example.fieldreserve.dataLayer.LocationRepository;
+import org.example.fieldreserve.model.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public List<Location> getAllLocations(){
+        return locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocationById(long id){
+        return locationRepository.findById(id);
+    }
+
+    public List<Location> findByCity(String city){
+        return locationRepository.findByCity(city);
+    }
+
+    public Location saveLocation(Location location){
+        return locationRepository.save(location);
+    }
+
+    public Location updateLocation(Long id, Location locationToUpdate){
+        Location existingLocation = locationRepository.findById(id).orElseThrow(()-> new RuntimeException("Location not found"));
+        existingLocation.setLocationArea(locationToUpdate.getLocationArea());
+        existingLocation.setLocationName(locationToUpdate.getLocationName());
+        existingLocation.setLocationCity(locationToUpdate.getLocationCity());
+        return locationRepository.save(existingLocation);
+    }
+
+}
